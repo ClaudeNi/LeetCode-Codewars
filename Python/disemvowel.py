4cec0374fb3871d2821879c7cc632a617f85b55a
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-08-31 20:26:10 +0300

    Delete Python directory

--- Python/disemvowel.py
+++
@@ -1,8 +0,0 @@
-def disemvowel(string_):
-    vowels = ["a","A","e","E","i","I","u","U","o","O"]
-    
-    str = ""
-    for char in string_:
-        if not char in vowels:
-            str = str + char
-    return str
