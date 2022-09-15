9fbdd76adc842759c7452c7dd80a18e55b182a1f
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-09-15 17:54:30 +0300

    Create isPowerOfTwo.js

---
+++ leetcode/javascript/isPowerOfTwo.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2^x.
+ */
+var isPowerOfTwo = function(n) {
+    if (n == 0) {
+        return false;
+    }
+    while (n % 2 == 0) {
+        n = n / 2;
+    }
+    if (n == 1) {
+        return true;
+    }
+    return false;
+};
